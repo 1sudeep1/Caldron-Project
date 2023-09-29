b30d52a2de1512ff855a59fdbc756c3d7264a02b
Sudeep Rajbanshi <isudp.th@gmail.com>
2023-09-29 12:52:32 +0545

    ScrollToTop and ScrollToTop1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Top from './Components/HeaderComponent/Top'
 import Header from './Components/HeaderComponent/Header'
 import Footer from './Components/FooterComponent/Footer'
 import FooterBottom from './Components/FooterComponent/FooterBottom'
+import ScrollToTop from 'react-scroll-to-top'
+import ScrollToTop1 from './Components/ScrollTopTop/ScrollToTop1'
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <Header/>
       <Footer/>
       <FooterBottom/>
+      <ScrollToTop smooth className='scroll-top' color='white' height='15' width='15' />
+      <ScrollToTop1/>
     </div>
   )
 }
